2c34b6ef1c1b77e5ce3aa75b66b6da5c41f0ba1e
Ignitor21 <jorik@DESKTOP-SS5P5SC.localdomain>
2022-08-18 17:53:07 +0300

    Использовал enum и рассмотрел случай 0 0 5

--- kvadratka.cpp
+++ kvadratka.cpp
@@ -1,39 +1,49 @@
 #include <stdio.h>
 #include <math.h>
 
+enum Event
+{
+OTHER,
+LINEAR1,
+LINEAR2,
+QUADRATIC
+};
+
 const double EPSILON = 0.000000001;
 float x1 = 0, x2 = 0, d = 0;
 float a = 0, b = 0, c = 0;
 
 void solution(float a, float b, float c);
-int event(float a, float b, float c);
-void printResult(int n);
+enum Event variant(float a, float b, float c);
+void printResult(enum Event n);
 
 int main(void)
-{
-	int variant = 0;
+{	
+	enum Event type;
 	printf("Программа решения квадратного уравнения\n");
 	printf("Введите 3 коэффициента квадратного уравнения(введите q для выхода из программы): ");
 	while (scanf("%f %f %f", &a, &b, &c) == 3)
 	{
-		variant = event(a, b, c);
-		if (variant == 2)
+		type = variant(a, b, c);	
+		if (type == QUADRATIC)
 			solution(a, b, c);
-		printResult(variant);	
-		printf("Введите через пробел 3 коэффициента квадратного уравнения(введите q для выхода из программы):\n");
+		printResult(type);	
+		printf("Введите через пробел 3 коэффициента квадратного уравнения(введите q для выхода из программы): ");
 	}
 	printf("Работа завершена :D\n");
 	return 0;
 }
 
-int event(float a,  float b, float c)
+enum Event variant(float a,  float b, float c)
 {
 	if (fabs(a) < EPSILON && fabs(b) < EPSILON && fabs(c) < EPSILON)
-		return 0;
+		return OTHER;
+	else if (fabs(a) < EPSILON && fabs(b) < EPSILON)
+		return LINEAR1;
 	else if (fabs(a) < EPSILON)
-		return 1;
+		return LINEAR2;
 	else
-		return 2;
+		return QUADRATIC;
 }
 
 void solution(float a, float b, float c)
@@ -49,20 +59,27 @@ void solution(float a, float b, float c)
 	
 }
 
-void printResult(int n)
+void printResult(enum Event n)
 {
-	if (n == 0)
-		printf("х - любое число.\n");
-	if (n == 1)
-		printf("Это линейное уравнение, его корень: %.2f\n", -c / b);
-	if (n == 2)
+	switch(n)
 	{
+	case OTHER:
+		printf("х - любое число.\n");
+		break;
+	case LINEAR1:
+		printf("Нет корней!\n");
+		break;
+	case LINEAR2:
+		printf("Корень этого уравнения: %.2f\n", -c / b);
+		break;
+	case QUADRATIC:
 		if (d > 0)
 			printf("Корни этого уравнения: %.2f и %.2f\n", x1, x2);
 		else if (fabs(d) < EPSILON)
 			printf("Корень этого уравнения: %.2f\n", x1);
 		else
 			printf("Нет корней!\n");
+		break;
 	}
 
 }
