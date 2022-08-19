5e14660632a572b9b929d7d47a9b8572fef30407
Ignitor21 <jorik@DESKTOP-SS5P5SC.localdomain>
2022-08-19 18:49:21 +0300

    Избавился от глобальных переменных и поправил вывод

--- kvadratka.cpp
+++ kvadratka.cpp
@@ -10,25 +10,24 @@ QUADRATIC
 };
 
 const double EPSILON = 0.000000001;
-float x1 = 0, x2 = 0, d = 0, d_sqrt = 0;
-float a = 0, b = 0, c = 0;
 
-void solution(float a, float b, float c);
 enum Event variant(float a, float b, float c);
-void printResult(enum Event n);
+void solution(float a, float b, float c, float* x1, float* x2, float* d);
+void printResult(enum Event n, float b, float c, float x1, float x2, float d);
 
 int main(void)
 {	
+	float x1 = 0, x2 = 0, d = 0;
+	float a = 0, b = 0, c = 0;
 	enum Event type;
 	printf("Программа решения квадратного уравнения\n");
-	printf("Введите 3 коэффициента квадратного уравнения(введите q для выхода из программы): ");
+	printf("Введите 3 коэффициента квадратного уравнения (введите q для выхода из программы): ");
 	while (scanf("%f %f %f", &a, &b, &c) == 3)
 	{
 		type = variant(a, b, c);	
-		if (type == QUADRATIC)
-			solution(a, b, c);
-		printResult(type);	
-		printf("Введите через пробел 3 коэффициента квадратного уравнения(введите q для выхода из программы): ");
+		solution(a, b, c, &x1, &x2, &d);
+		printResult(type, b, c , x1, x2, d);	
+		printf("Введите через пробел 3 коэффициента квадратного уравнения (введите q для выхода из программы): ");
 	}
 	printf("Работа завершена :D\n");
 	return 0;
@@ -46,21 +45,22 @@ enum Event variant(float a,  float b, float c)
 		return QUADRATIC;
 }
 
-void solution(float a, float b, float c)
+void solution(float a, float b, float c, float* x1, float* x2, float* d)
 {
-	d = b*b - 4 * a * c, 0.5;
-	d_sqrt = pow(d, 0.5);
-	if (d_sqrt > 0)
+	float d_sqrt = 0;
+	*d = b*b - 4 * a * c;
+	d_sqrt = pow(*d, 0.5);
+	if (*d > 0)
 	{
-		x1 = (-b - d_sqrt) / (2 * a);
-		x2 = (-b + d_sqrt) / (2 * a);
+		*x1 = (-b - d_sqrt) / (2 * a);
+		*x2 = (-b + d_sqrt) / (2 * a);
 	}
-	else if (fabs(d) < EPSILON)
-		x1 = x2 = -b / (2 * a);
+	else if (fabs(*d) < EPSILON)
+		*x1 = *x2 = -b / (2 * a);
 	
 }
 
-void printResult(enum Event n)
+void printResult(enum Event n, float b, float c, float x1, float x2, float d)
 {
 	switch(n)
 	{
@@ -71,13 +71,13 @@ void printResult(enum Event n)
 		printf("Нет корней!\n");
 		break;
 	case LINEAR2:
-		printf("Корень этого уравнения: %.2f\n", -c / b);
+		printf("Корень этого уравнения: %.2f\n", fabs(c) < EPSILON ? 0: -1 * c / b);
 		break;
 	case QUADRATIC:
 		if (d > 0)
 			printf("Корни этого уравнения: %.2f и %.2f\n", x1, x2);
 		else if (fabs(d) < EPSILON)
-			printf("Корень этого уравнения: %.2f\n", x1);
+			printf("Корень этого уравнения: %.2f\n", (fabs(b) < EPSILON && fabs(c) < EPSILON) ? 0 : x1);
 		else
 			printf("Нет корней!\n");
 		break;
