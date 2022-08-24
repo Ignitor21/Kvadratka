b3758fbb164891ef773077843980510a477d5de9
Ignitor21 <jorik@DESKTOP-SS5P5SC.localdomain>
2022-08-24 14:42:47 +0300

    Много чего сделал, мне лень это описывать(так как лш :D)

--- functions.cpp
+++ functions.cpp
@@ -1,28 +1,31 @@
 #include <stdio.h>
 #include <math.h>
-#include "Header.h"
+#include "prototypes.h"
 
-bool isEqual(const float a, const float b)
+const double EPSILON = 10E-8;
+
+bool isEqual(const double a, const double b)
 {
 	return (fabs(a-b) < EPSILON);
 }
 
-enum variant solution(const float a, const float b, const float c, float* x1, float* x2)
+enum variant linearSolution(const double a, const double b, const double c, double* x1, double* x2)
 {
-	if (isEqual(a, 0) && isEqual(b, 0) && isEqual(c, 0))
+	if (isEqual(b, 0) && isEqual(c, 0))
 		return OTHER;
-	else if (isEqual(a, 0) && isEqual(b, 0))
+	else if (isEqual(b, 0))
 		return NO_SOLUTIONS;
-	else if (isEqual(a, 0))
+	else 
 	{
 		*x1 = *x2 = -c / b;
 		return LINEAR;
 	}
-	else
-	{
-		float d = 0, d_sqrt = 0;
+}
+
+enum variant squareSolution(const double a, const double b, const double c, double* x1, double* x2)
+{
+		double d = 0, d_sqrt = 0;
 		d = b * b - 4 * a * c;
-		d_sqrt = sqrt(d);
 
 		if (d < 0)
 			return QUADRATIC_0;
@@ -33,14 +36,14 @@ enum variant solution(const float a, const float b, const float c, float* x1, fl
 		}
 		else
 		{
+			d_sqrt = sqrt(d);
 			*x1 = (-b + d_sqrt) / (2 * a);
 			*x2 = (-b - d_sqrt) / (2 * a);
 			return QUADRATIC_2;
 		}
-	}
 }
 
-void printResult(const enum variant n, const float x1, const float x2)
+void printResult(const enum variant n, const double x1, const double x2)
 {
 	switch(n)
 	{
@@ -51,27 +54,48 @@ void printResult(const enum variant n, const float x1, const float x2)
 		printf("Нет корней!\n");
 		break;
 	case LINEAR:
-		printf("Корень этого уравнения: %.2f\n", x1);
+		printf("Корень этого уравнения: %lg\n", x1);
 		break;
 	case QUADRATIC_0:
 		printf("Нет корней!\n");
 		break;
 	case QUADRATIC_1:
-		printf("Корень этого уравнения: %.2f\n", x1);
+		printf("Корень этого уравнения: %lg\n", x1);
 		break;
 	case QUADRATIC_2:
-		printf("Корни этого уравнения: %.2f %.2f\n", x1, x2);
+		printf("Корни этого уравнения: %lg и %lg\n", x1, x2);
 		break;
 	}
 }
 
-void isInputCorrect(float* a, float* b, float* c)
+void isDoubleInputCorrect(double* a)
 {
-	while (scanf("%f %f %f", a, b, c) != 3)
+	while (scanf("%lg", a) != 1)
 	{
-		printf("Некорректный ввод! Введите 3 числа: ");
+		printf("Некорректный ввод! Повторите попытку: ");
 		while (getchar() != '\n')
 			continue;
 	}
+	while (getchar() != '\n')
+			continue;
 }
 
+void isIntInputCorrect(int* a)
+{
+	while (scanf("%d", a) != 1)
+	{
+		printf("Некорректный ввод! Введите 1 или 2: ");
+		while (getchar() != '\n')
+			continue;
+	}
+	while (getchar() != '\n')
+			continue;
+}
+
+void printMenu(void)
+{
+	printf("********************************************************************************\n");
+	printf("           Введите одно из следующих чисел, чтобы продолжить:\n");
+	printf("   1. Начать решение квадратного уравнения          2. Выход из программы\n");
+	printf("********************************************************************************\n");
+}
--- kvadratka.cpp
+++ kvadratka.cpp
@@ -1,19 +1,40 @@
 #include <stdio.h>
-#include "Header.h"
+#include "prototypes.h"
 
 int main(void)
 {	
-	float x1 = 0, x2 = 0;
-	float a = 0, b = 0, c = 0;
+	double a = 0, b = 0, c = 0, x1 = 0, x2 = 0;
+	int response = 0;
 	enum variant type = {};
+	enum step action = {};
+	printf("Вас привествуете программа решения квадратного уравнения!\n");
+	printMenu();
+	while(true)
+	{
+		isIntInputCorrect(&response);
+		if (response == SOLVE)
+		{
+			printf("Введите 3 коэффициента квадратного уравнения: \n");
+			printf("Введите коэффициент a: ");
+			isDoubleInputCorrect(&a);
+			printf("Введите коэффициент b: ");
+			isDoubleInputCorrect(&b);
+			printf("Введите коэффициент c: ");
+			isDoubleInputCorrect(&c);
 
-	printf("Программа решения квадратного уравнения\n");
-	printf("Введите 3 коэффициента квадратного уравнения (введите q для выхода из программы): ");
+			if (isEqual(a, 0))
+				type = linearSolution(a, b, c, &x1, &x2);
+			else
+				type = squareSolution(a, b, c, &x1, &x2);
 
-	isInputCorrect(&a, &b, &c);
-	type = solution(a, b, c, &x1, &x2);
-	printResult(type, x1, x2);	
-	//printf("Введите 3 коэффициента квадратного уравнения (введите q для выхода из программы): ");
+			printResult(type, x1, x2);
+			printMenu();
+		}
+		else if (response == FINISH)
+			break;
+		else 
+			printf("Некорректный ввод! Введите 1 или 2: ");
+	}
 	printf("Работа завершена :D\n");
 
 	return 0;
